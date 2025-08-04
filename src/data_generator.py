a1471dc45fe82a78abeebf7bbdcc9677f592016b
daspushpita <pushpitads1996@gmail.com>
2025-08-03 22:04:07 -0400

    Adding the PINN model

---
+++ src/data_generator.py
@@ -0,0 +1,89 @@
+import os, sys
+import numpy as np
+import pandas as pd
+
+    
+class data_generator():
+    """
+    Reads 1D PDE solution snapshots from disk and returns X → Y training data,
+    optionally skipping time steps and holding out test data.
+    """
+    def __init__(self, path, holdout, nsteps, dim, csv=1, npz=0, x_res=100, gen=0, skip=0, *args, **kwargs):
+        
+        self.filename = path
+        self.dim = dim
+        self.csv = csv
+        self.npz = npz
+        self.nx = x_res
+        self.gen = gen
+        self.holdout = holdout
+        self.skip = skip
+        self.nsteps = nsteps
+
+    def read_1d(self):
+        
+        if (self.dim != 1):
+            raise ValueError ("1D read function called for Dimensions > 1")
+        
+        if (self.csv):
+            files = sorted([f for f in os.listdir(self.filename) if f.startswith('data_') and f.endswith('.csv')])
+            files = files if self.skip==0 else files[::self.skip]
+            x_shape = self.nx
+            data = pd.read_csv(os.path.join(self.filename, files[0]))
+            self.x = data["x"].values
+            nt = len(files)
+            var_u_all = np.zeros((nt, x_shape, 3))
+            for i, file in enumerate(files):
+                data = pd.read_csv(os.path.join(self.filename, file))
+                var_u = data.filter(like='u_t').values.flatten()
+                var_x = data["x"]
+                var_t = data["t"].iloc[0]
+
+                var_u_all[i, :, 0] = var_u
+                var_u_all[i, :, 1] = var_t
+                var_u_all[i, :, 2] = var_x
+                                
+        elif (self.npz):
+            data = np.load(self.filename)
+            var_u = data['u'][:-1]
+            var_t = data['time'][:-1]
+            var_x = data['x']
+            nt = len(var_t)
+            x_shape = self.nx
+            var_u_all = np.zeros((nt, x_shape, 3))
+
+            var_u_all[:, :, 0] = var_u
+            var_u_all[:, :, 1] = var_t            
+            var_u_all[:, :, 2] = var_x
+        
+        else:
+            raise ValueError ("No datafile format specified!")
+            
+            
+        X = var_u_all[:-(self.nsteps), :, :]  # shape: (nt - nsteps, nx)
+
+        # Build multi-step Y
+        Y = np.stack([var_u_all[i+1:i+1+self.nsteps, :, 0]  # shape: (nsteps, nx)
+            for i in range(nt - self.nsteps)])
+        Y = np.transpose(Y, (0, 2, 1))  # shape: (samples, nx, nsteps)
+        
+        # Split training vs held-out
+        if self.holdout > 0:
+            X_train = X[:-self.holdout]
+            Y_train = Y[:-self.holdout]
+            X_test = X[-self.holdout:]
+            Y_test = Y[-self.holdout:]
+        else:
+            X_train, Y_train = X, Y
+            X_test, Y_test = None, None  # or np.empty(...)
+        
+        # column_names = [f"u{i}" for i  in range(self.nx)]
+        # X_train_df = pd.DataFrame(X_train, columns=column_names)
+        # Y_train_df = pd.DataFrame(Y_train, columns=column_names)
+        
+        if self.gen:
+            x_grid = self.x
+            return X_train, Y_train, X_test, Y_test, x_grid
+        else:
+            return X_train, Y_train, X_test, Y_test
+    
---
+++ src/model.py
@@ -0,0 +1,284 @@
+import os, sys
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models, Input, regularizers
+
+
+class cnn_models():
+    """
+    A class that defines 1D Convolutional Neural Network models for learning PDE dynamics,
+    specifically designed to map u(x, t) → u(x, t+Δt) for time-evolving spatial systems.
+
+    Attributes:
+        nx (int): Number of spatial grid points in the input profile.
+    """
+    def __init__(self, nx, dim, *args, **kwargs):
+        """
+        Initializes the model class with grid resolution.
+
+        Args:
+            nx (int): Number of spatial grid points in the input data.
+        """
+        self.nx = nx
+        self.dim = dim
+        
+    def cnn_model_1d_single_diff(self, K):
+        """
+        Builds a 1D CNN model that maps an input field u(x)^n to the next time step u(x)^{n+1}.
+        The model preserves spatial structure using only convolutional layers.
+
+        Returns:
+            model_cnn (tf.keras.Model): Compiled 1D CNN model ready for training.
+        """
+        # Input shape: (nx, 1) — scalar value at each grid point
+        inputs = Input(shape=(self.nx,1), name = 'input_layer')
+        x = layers.Conv1D(6, kernel_size=3, padding='same', activation='relu', name='conv_l1')(inputs)
+        x = layers.Conv1D(6, kernel_size=3, padding='same', activation='relu',name='conv_l2')(x)
+        if K > 1:
+            x = layers.Conv1D(6, kernel_size=3, padding='same', activation='relu',name='conv_l3')(x)
+        output = layers.Conv1D(K, kernel_size=1, padding='same', name='output')(x)
+        model_cnn = models.Model(inputs=inputs, outputs=output)
+        return model_cnn
+    
+    def cnn_model_1d_multistep_diff(self, K):
+        """
+        Builds a 1D CNN model that maps an input field u(x)^n to multiple next time steps u(x)^{n+1}, u(x)^{n+2}, u(x)^{n+3}.
+        The model preserves spatial structure using only convolutional layers.
+
+        Returns:
+            model_cnn (tf.keras.Model): Compiled 1D CNN model ready for training.
+        """
+        # Input shape: (nx, 1) — scalar value at each grid point
+        inputs = Input(shape=(self.nx, self.dim), name = 'input_layer')
+        x = layers.Conv1D(9, kernel_size=3, padding='same', activation='relu', name='conv_l1')(inputs)
+        x = layers.Conv1D(9, kernel_size=3, padding='same', activation='relu',name='conv_l2')(x)
+        if K > 1:
+            x = layers.Conv1D(6, kernel_size=3, padding='same', activation='relu',name='conv_l3')(x)
+            
+        x = layers.TimeDistributed(layers.Dense(136, activation='relu'), name='neural')(x)
+        output = layers.Conv1D(K, kernel_size=1, padding='same', name='output')(x)
+        model_cnn = models.Model(inputs=inputs, outputs=output)
+        return model_cnn
+
+    def cnn_model_1d_gen_diff(self):
+        """
+        Builds a 1D CNN model that maps an input field u(x)^n to the next time step u(x)^{n+1}.
+        The model preserves spatial structure using only convolutional layers.
+
+        Returns:
+            model_cnn (tf.keras.Model): Compiled 1D CNN model ready for training.
+        """
+        # Input shape: (nx, 2) — scalar value at each grid point
+        inputs = Input(shape=(self.nx,10), name = 'input_layer')
+        # Convolutional layers to extract spatial features
+        x = layers.Conv1D(32, kernel_size=5, padding='same', activation='tanh', name='conv1')(inputs)
+        x = layers.Conv1D(32, kernel_size=5, padding='same', activation='tanh', name='conv2')(x)
+        x = layers.Conv1D(6, kernel_size=5, padding='same', activation='tanh', name='conv3')(x)
+        x = layers.BatchNormalization(name='batchnorm')(x)
+        x = layers.Flatten(name='flatten')(x)
+        x = layers.Dense(128, activation='tanh', name='dense1')(x)
+        x = layers.Dense(self.nx, activation='linear', name='dense2')(x)
+        output = layers.Reshape((self.nx, 1), name='output')(x)
+
+        # Define the model
+        model_cnn = models.Model(inputs=inputs, outputs=output, name='cnn_pos_enc_model')
+        return model_cnn
+    
+
+class pinn_models():
+    """
+    A class that defines  Physics-informed Neural Network models for learning PDE dynamics.
+
+    Attributes:
+        nx (int): Number of spatial grid points in the input profile.
+    """
+    def __init__(self, data_path, resolution, 
+                 N_IC, N_BC, N_CP, 
+                 epoch=1000, 
+                 learning_rate=1.e-3,
+                 *args, **kwargs):
+        """
+        Initializes the model class with grid resolution.
+
+        Args:
+        """
+        self.data_path = data_path
+        self.resolution = resolution
+        self.N_IC = N_IC
+        self.N_BC = N_BC
+        self.N_CP = N_CP
+        self.epoch=epoch
+        self.learning_rate= learning_rate
+        np.random.seed(42)  # Set seed once
+        
+    def data_generator(self, IC, BC, CP):
+        
+        if IC==True:
+            data = pd.read_csv(self.data_path+'data_0000.csv')
+            x = data['x']
+            u_xt = data['u_t0']
+            idx = np.random.choice(len(x), size=self.N_IC, replace=False)
+            x_ic = x[idx]
+            t_ic = np.zeros_like(x_ic)
+            u_xt_ic = u_xt[idx]
+            x_tf_ic = tf.convert_to_tensor(x_ic, dtype=tf.float32)
+            t_tf_ic = tf.convert_to_tensor(t_ic, dtype=tf.float32)
+            u_tf_ic = tf.convert_to_tensor(u_xt_ic, dtype=tf.float32)
+            X_input = tf.concat([x_tf_ic, t_tf_ic], axis=1)
+            return X_input, u_tf_ic
+        
+        else:
+            files = sorted([f for f in os.listdir(self.data_path) if f.startswith('data_') and f.endswith('.csv')])
+            nt = len(files) - 1 #Ignoring the initial condition
+            time_array = np.zeros((nt))
+            var_u_all = np.zeros((nt, self.resolution, 3))
+            for i, file in enumerate(files):
+                data_all = pd.read_csv(os.path.join(self.data_path, file))
+                var_u = data.filter(like='u_t').values.flatten()
+                var_t = data_all["t"].iloc[0]
+                var_x = data["x"]
+                time_array[i] = var_t
+                var_u_all[i, :, 0] = var_u
+                var_u_all[i, :, 1] = var_t
+                var_u_all[i, :, 2] = var_x
+            
+            if BC == True:
+                idt = np.random.choice(len(time_array), size=self.N_BC, replace=False)
+                t_bc = time_array[idt]
+                x_bc = np.random.choice([0.0, 1.0], size=self.N_BC)
+                u_bc = np.zeros_like(x_bc)
+                
+                for i in range(self.N_BC):
+                    if x_bc[i] == 0.0:
+                        u_bc[i] = var_u_all[idt[i], 0, 0]  # u at x=0
+                    else:
+                        u_bc[i] = var_u_all[idt[i], -1, 0]  # u at x=1
+
+                # Convert to tensors
+                x_tf_bc = tf.convert_to_tensor(x_bc[:, None], dtype=tf.float32)
+                t_tf_bc = tf.convert_to_tensor(t_bc[:, None], dtype=tf.float32)
+                u_tf_bc = tf.convert_to_tensor(u_bc[:, None], dtype=tf.float32)
+                X_input = tf.concat([x_tf_bc, t_tf_bc], axis=1)
+                return X_input, u_tf_bc
+            
+            elif CP == True:
+                x_cp = np.random.uniform(0.0, 1.0, size=self.N_CP)
+                t_cp = np.random.uniform(np.min(time_array), np.max(time_array), size=self.N_CP)
+
+                # Convert to tensors
+                x_tf_cp = tf.convert_to_tensor(x_cp[:, None], dtype=tf.float32)
+                t_tf_cp = tf.convert_to_tensor(t_cp[:, None], dtype=tf.float32)
+
+                X_input = tf.concat([x_tf_cp, t_tf_cp], axis=1)
+                return X_input
+            
+            else:
+                raise ValueError("Train Data type not specified")
+
+    def build_pinnmodel(self, n_hidden=4, n_neurons=64):
+        
+        inputs = Input(shape=(2,), name = 'input_layer')
+        x = inputs
+        for i in range(n_hidden):
+            x = layers.Dense(n_neurons, activation='tanh', name=f'dense_{i+1}')(x)
+        output = layers.Dense(1, activation=None, name='output')(x)
+        mymodel = models.Model(inputs=inputs, outputs=output, name='PINN')
+        self.model_cont = mymodel
+        return mymodel
+    
+    def pde_residual(self, diff_const, X_tensor):
+
+        x_tf = X_tensor[:, 0:1]
+        t_tf = X_tensor[:, 1:2]
+        X_input = tf.concat([x_tf, t_tf], axis=1)  # shape (N, 2)
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch([x_tf, t_tf])
+            u = self.model_cont(X_input)
+            u_x = tape.gradient(u, x_tf)
+            u_xx = tape.gradient(u_x, x_tf)
+            u_t = tape.gradient(u, t_tf)
+        
+        f = u_t - diff_const * u_xx
+        return f
+    
+    def loss_terms(self, diff_const):
+        
+        X_ic_input, u_ic = self.data_generator(IC=True, BC=False, CP=False)
+        u_ic_pred = self.model_cont (X_ic_input)
+        loss_ic = tf.reduce_mean(tf.square(u_ic - u_ic_pred))
+        
+        X_bc_input, u_bc = self.data_generator(IC=False, BC=True, CP=False)
+        u_bc_pred = self.model_cont (X_bc_input)
+        loss_bc = tf.reduce_mean(tf.square(u_bc - u_bc_pred))
+        
+        X_cp_input = self.data_generator(IC=False, BC=False, CP=True)
+        loss_pde = tf.reduce_mean(tf.square(self.pde_residual(diff_const, X_cp_input)))
+        
+        loss = loss_ic + loss_bc + loss_pde
+        return loss, loss_ic, loss_bc, loss_pde
+    
+    def train_model(self, diff_const):
+        
+        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        for epoch in range(self.epoch):
+            with tf.GradientTape() as tape:
+                loss, loss_ic, loss_bc, loss_pde = self.loss_terms(diff_const)
+            
+            grads = tape.gradient(loss, self.model_cont.trainable_variables)
+            optimizer.apply_gradients(zip(grads, self.model_cont.trainable_variables))
+            if epoch % 100 == 0:
+                print(f"Epoch {epoch}: Total Loss = {loss.numpy():.4e} | IC = {loss_ic.numpy():.4e} | BC = {loss_bc.numpy():.4e} | PDE = {loss_pde.numpy():.4e}")
+        
+    def predict(self, x, t):        
+        # Ensure x and t are column vectors
+        x = np.reshape(x, (-1, 1))
+        t = np.reshape(t, (-1, 1))
+        
+        # Convert to TensorFlow tensors
+        x_tf = tf.convert_to_tensor(x, dtype=tf.float32)
+        t_tf = tf.convert_to_tensor(t, dtype=tf.float32)
+        # Concatenate inputs
+        X_input = tf.concat([x_tf, t_tf], axis=1)
+
+        # Make prediction
+        u_pred = self.model_cont(X_input)
+        return u_pred
+    
+    def save_model(self, save_path):
+        """
+        Save the trained model weights to a given path.
+        """
+        self.model_cont.save_weights(save_path)
+        print(f"Model weights saved to {save_path}")
+
+    def load_model(self, model_architecture, load_path):
+        """
+        Load model weights into a given model architecture.
+
+        Args:
+            model_architecture: a model instance created by build_pinnmodel(...)
+            load_path: path to the saved weights
+        """
+        model_architecture.load_weights(load_path)
+        self.model_cont = model_architecture
+        print(f"Model weights loaded from {load_path}")
+
+
+class Positional_Encoding():
+    
+    def __init__(self, x, d_model, n_user=10000, *args, **kwargs):
+        self.x = x
+        self.d_model = d_model
+        self.n_user = n_user
+    
+    def sinosoidal_encoding(self):
+        nk = len(self.x)
+        n_freq = self.d_model//2
+        pe_matrix = np.zeros((nk, self.d_model))
+        for i in range(n_freq):
+            theta = self.x / (self.n_user ** (2 * i / self.d_model))
+            pe_matrix[:, 2*i] = np.sin(theta)
+            pe_matrix[:, 2*i+1] = np.cos(theta)
+        return pe_matrix
+    
